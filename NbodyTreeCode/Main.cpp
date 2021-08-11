7c689cdfe7544ce5bef57f7ef2bf148696b618ea
Ivan Xu <info@chivalrystudio.com>
2021-08-11 10:27:48 -0700

    done

--- NbodyTreeCode/Main.cpp
+++ NbodyTreeCode/Main.cpp
@@ -1,4 +1,12 @@
-﻿#include "NbodyTreeCode.h"
+﻿#include <algorithm>
+#include <array>
+#include <execution>
+#include <iostream>
+#include <random>
+#include <complex>
+
+#include "AdaptiveQuadtree.h"
+#include "Body.h"
 
 double my_rand()
 {
