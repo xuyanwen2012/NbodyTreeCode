b313e3c8c2a9d1bf8e4ca75046220a45cbcc1ede
Ivan Xu <info@chivalrystudio.com>
2021-08-11 10:04:08 -0700

    Add project files.

---
+++ NbodyTreeCode/NbodyTreeCode.cpp
@@ -0,0 +1,12 @@
+﻿// NbodyTreeCode.cpp : Defines the entry point for the application.
+//
+
+#include "NbodyTreeCode.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
