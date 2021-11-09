afc2affde6165226a9627ce78ee20975374e6a26
Ivan Xu <info@chivalrystudio.com>
2021-11-09 10:50:48 -0800

    modify kernel

--- NbodyTreeCode/Body.h
+++ NbodyTreeCode/Body.h
@@ -26,6 +26,16 @@ struct body
 template <typename T>
 std::complex<T> kernel_func(const std::complex<T>& i, const std::complex<T>& j)
 {
-	const std::complex<T> dist = i - j;
-	return dist / pow(abs(dist), 3);
+	static constexpr T softening = 1e-9;
+
+	//const std::complex<T> dist = i - j;
+	T dx = i.real() - i.imag();
+	T dy = i.imag() - j.imag();
+
+	//return dist / pow(abs(dist), 3);
+	T dist_sqr = dx * dx + dy * dy + softening;
+	T inv_dist = 1.0 / sqrt(dist_sqr);
+	T inv_dist3 = inv_dist * inv_dist * inv_dist;
+
+	return {dx * inv_dist3, dy * inv_dist3};
 }
