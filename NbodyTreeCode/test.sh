b9385000f7f1f59bb00f3b7afa2fd5297f1de7ba
Ivan Xu <info@chivalrystudio.com>
2021-11-11 15:17:13 -0800

    save

--- NbodyTreeCode/test.sh
+++ NbodyTreeCode/test.sh
@@ -3,7 +3,7 @@
 rm tmp_new.txt
 rm mlpStats memStats epochStats
 make
-./decades_base/decades_base 1024
+./decades_base/decades_base
 ../../MosaicSim_private/tools/mosaicrun -cc yanwen_inorder . > tmp_new.txt
 cat tmp_new.txt
 python3 show_mem.py
